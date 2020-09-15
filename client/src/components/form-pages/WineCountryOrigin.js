edbfdeb09ca91c7ea147dee02f1035b7e7efc200
Chris <cgcottier@gmail.com>
2020-09-15 11:48:08 -0500

    debugging

--- client/src/components/form-pages/WineCountryOrigin.js
+++ client/src/components/form-pages/WineCountryOrigin.js
@@ -1,24 +1,47 @@
 import React from "react";
-import {Redirect} from 'react-router-dom'
+import { Redirect } from "react-router-dom";
 
 import FormPage from "../FormPage";
 
 const WineCountryOrigin = () => {
-
-
   return (
     <>
       <FormPage
-        category='country'
+        category="country"
         options={[
-          { optionText: "United States", optionValue: 'USA', optionPic: <img src='https://img.icons8.com/ios/100/000000/usa-map.png' /> },
-          { optionText: "France", optionValue: 'France', optionPic: <img src="https://img.icons8.com/ios/100/000000/france-map.png"/>},
-          { optionText: "Spain", optionValue: 'Spain', optionPic: <img src="https://img.icons8.com/ios/100/000000/spain-map.png"/> },
+          {
+            optionText: "United States",
+            optionValue: "USA",
+            optionPic: (
+              <img src="https://img.icons8.com/ios/100/000000/usa-map.png" />
+            ),
+          },
+          {
+            optionText: "France",
+            optionValue: "France",
+            optionPic: (
+              <img src="https://img.icons8.com/ios/100/000000/france-map.png" />
+            ),
+          },
+          {
+            optionText: "Spain",
+            optionValue: "Spain",
+            optionPic: (
+              <img src="https://img.icons8.com/ios/100/000000/spain-map.png" />
+            ),
+          },
+          {
+            optionText: "Canada",
+            optionValue: "Canada",
+            optionPic: (
+              <img src="https://img.icons8.com/ios/100/000000/spain-map.png" />
+            ),
+          },
         ]}
         question={"Do you care where the wine is from?"}
         defaultOutput={"I'm looking for a wine from"}
         canChooseMultiple={true}
-        type='selections'
+        type="selections"
         // previous={null}
         // next='redirect'
       ></FormPage>
@@ -26,8 +49,10 @@ const WineCountryOrigin = () => {
   );
 };
 
-{/* <a href="https://icons8.com/icon/45746/usa-map">USA Map icon by Icons8</a>
+{
+  /* <a href="https://icons8.com/icon/45746/usa-map">USA Map icon by Icons8</a>
 <a href="https://icons8.com/icon/58460/france-map">France Map icon by Icons8</a>
-<a href="https://icons8.com/icon/58505/spain-map">Spain Map icon by Icons8</a> */}
+<a href="https://icons8.com/icon/58505/spain-map">Spain Map icon by Icons8</a> */
+}
 
 export default WineCountryOrigin;
--- client/src/reducers/forms.js
+++ client/src/reducers/forms.js
@@ -1,15 +1,21 @@
-import {SET_FORM, SET_SELECTION, NEW_FORM, CHANGE_PAGE} from '../actions/forms'
+import {
+  SET_FORM,
+  SET_SELECTION,
+  NEW_FORM,
+  CHANGE_PAGE,
+} from "../actions/forms";
 
 const defaultState = {
-    form: null,
-    pageNum:1,
-    selections: {
-        country: [],
-        price: [],
-        redWineVerietal: [],
-        wineRating: [],
-        wineNotes: []
-    }
+  form: null,
+  pageNum: 1,
+  country: [],
+  selections: {
+    country: [],
+    price: [],
+    redWineVerietal: [],
+    wineRating: [],
+    wineNotes: [],
+  },
 };
 
 const forms = (state = defaultState, action) => {
@@ -30,54 +36,55 @@ const forms = (state = defaultState, action) => {
     //     }
     // }
 
-      case SET_SELECTION: {
-        // const newCountry = [...state.selections.country];
-        // newCountry.push(action.value)
-        // return Object.assign({}, state, {selections: {
-        //   country: [...newCountry],
-        //   price: [] 
-        // }})
-        let newSelections = Object.assign({}, state.selections);
-        const {category, value} = action
-        // debugger;
-        let array = newSelections[category];
-        const indexOfValue = array.indexOf(value);
+    case SET_SELECTION: {
+      const { category, value } = action;
+      // debugger;
+      let array = state.selections[category];
+      array = [...array];
+      const indexOfValue = array.indexOf(value);
 
-        if (indexOfValue === -1) {
-          array = [...array, value]
+      if (indexOfValue === -1) {
+        array = array.concat(value);
+        //  = [...array, value];
+      } else {
+        debugger;
+        array.splice(indexOfValue, 1);
+      }
 
-        }
-        //  else {
+      const retVal2 = Object.assign({}, state, {
+        selections: {
+          ...state.selections,
+          [category]: array,
+        },
+      });
+      debugger;
+      return retVal2;
+      // const retVal = {
+      //   ...state,
+      //   country: state.country.concat(value),
+      //   selections: {
+      //     ...state.selections,
+      //     [category]: state.country.concat(value),
+      //   },
+      // };
+      // // debugger;
+      // return retVal;
+    }
 
-        //   array.splice(indexOfValue, 1)
-        // }
-
-        newSelections[category] = array
-        // debugger;
-
-
-        return {
-            ...state,
-            selections: {...newSelections},
-            }
-        }
-    
-
-    
     case NEW_FORM: {
-        return {
-            ...state,
-            form: action.formUrl,
-            pageNum: 1,
-            selections: defaultState.selections
-        }
+      return {
+        ...state,
+        form: action.formUrl,
+        pageNum: 1,
+        selections: defaultState.selections,
+      };
     }
 
     case CHANGE_PAGE: {
       return {
         ...state,
-        pageNum: action.value
-      }
+        pageNum: action.value,
+      };
     }
 
     default:
@@ -86,3 +93,32 @@ const forms = (state = defaultState, action) => {
 };
 
 export default forms;
+
+// case SET_SELECTION: {
+//   // const newCountry = [...state.selections.country];
+//   // newCountry.push(action.value)
+//   // return Object.assign({}, state, {selections: {
+//   //   country: [...newCountry],
+//   //   price: []
+//   // }})
+//   let newSelections = Object.assign({}, state.selections);
+//   const { category, value } = action;
+//   // debugger;
+//   let array = newSelections[category];
+//   array = [...array];
+//   const indexOfValue = array.indexOf(value);
+
+//   if (indexOfValue === -1) {
+//     array = [...array, value];
+//   } else {
+//     array.splice(indexOfValue, 1);
+//   }
+
+//   newSelections[category] = array;
+//   // debugger;
+
+//   return {
+//     ...state,
+//     selections: { ...newSelections },
+//   };
+// }
