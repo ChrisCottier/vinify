18cb4ba2ecae8e881742c0652b1d6b7b462f5885
Chris <cgcottier@gmail.com>
2020-09-30 18:01:51 -0500

    scraping wine data for location from wine fetch in new api route

--- app/api/wine_routes.py
+++ app/api/wine_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
 from sqlalchemy import text
+import urllib
+from bs4 import BeautifulSoup
 
 from app.models import db
 from app.models.wines import Wine
@@ -57,4 +59,48 @@ def wine_details(id):
 
     wine['user_follows'] = user_follows
 
+    return jsonify(wine)
+
+
+@wine_routes.route('/<id>/finder')
+def wine_finder(id):
+    wine = Wine.query.get(id).to_dict()
+
+    # we have the wine, so now we'll search it with wine finder
+    wine_kws = '+'.join(wine['name'].split(' '))
+
+    # we parse an html request for the wine
+    url = f'https://www.winefetch.com/websearch_results.html?site=&kw={wine_kws}&year=vintage&size='
+    web_url = urllib.request.urlopen(url)
+    data = web_url.read()
+    soup = BeautifulSoup(data, 'html.parser')
+
+    # grab name
+    # print(soup.select('td.wfc > b'))
+    item_name_arr = soup.select('td.wfc > b')[3:]
+    names = [name.get_text() for name in item_name_arr]
+    # print(names)
+
+    # grab shop name
+    shop_name_arr = soup.select('td > a.wf_content_link')[3:]
+    shop_names = [shop_name.get_text() for shop_name in shop_name_arr]
+
+    # grab shop location
+    shop_location_arr = soup.select('td[width="10"] + td')
+    shop_locations = [shop_location.get_text('|')
+                      for shop_location in shop_location_arr][:-1]
+    shop_locations_parsed = [
+        location[location.index('|') + 1:] for location in shop_locations]
+    # print('shop loc', shop_locations_parsed)
+
+    # grab buy link
+    buy_link_arr = soup.select('td[class="retail"] > a[rel="nofollow"]')
+    buy_links = [buy_link['href'] for buy_link in buy_link_arr]
+    buy_links_parsed = [
+        f'https://www.winefetch.com{buy_link}' for buy_link in buy_links]
+    print('buy', buy_links_parsed)
+
+    # print(shop_names)
+
+    # print(soup.select('td.wfc > b'))
     return jsonify(wine)
