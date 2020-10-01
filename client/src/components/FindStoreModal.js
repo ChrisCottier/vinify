f325a4c204535f4dbea0571bbad1434aec8ccd6f
Chris <cgcottier@gmail.com>
2020-10-01 12:21:26 -0500

    wine store finder api, modal completed

--- client/src/components/FindStoreModal.js
+++ client/src/components/FindStoreModal.js
@@ -3,29 +3,22 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { FIND_WINE_MODAL } from "../actions/modals";
 import { findStores } from "../actions/wines";
+import Loading from "./Loading";
 
 //FAICON {tag, type, size}
 
 const FindStoreModal = ({ id }) => {
   const dispatch = useDispatch();
   const { findWineDisplay } = useSelector((state) => state.modals);
-  const { stores } = useSelector((state) => state.wines);
+  const { stores, wineStoresId } = useSelector((state) => state.wines);
 
   const [locationsUpdated, setLocationsUpdated] = useState(false);
-
-  // useEffect(() => {
-  //   // TODO as is this functions to preload modl on every wine page,
-  //   // instead perhaps have the data load only once on Find It! click
-  //   if (locationsUpdated) return;
-  //   dispatch(findStores(id));
-  //   setLocationsUpdated(true);
-  // });
 
   const modalOff = () => {
     dispatch({ type: FIND_WINE_MODAL, display: "none" });
   };
 
-  if (!findWineDisplay || !stores) return null;
+  if (!findWineDisplay || !stores || !wineStoresId) return null;
   return (
     <div
       id="sign-up-modal"
@@ -39,39 +32,52 @@ const FindStoreModal = ({ id }) => {
         </header>
         <section className="modal-card-body">
           <div id="matching-store-container">
-            {stores.map((store, ind) => {
-              return (
-                <div key={ind} className="wine-store">
-                  <div>
-                    <span className="wine-store-details">List Item:</span>{" "}
-                    {store.wine_name}
-                  </div>
-                  <div>
-                    <span className="wine-store-details">List Price:</span>{" "}
-                    {store.list_price}
-                  </div>
-                  <div>
-                    <span className="wine-store-details">Store:</span>{" "}
-                    {store.wine_shop}
-                  </div>
-                  <div>
-                    <span className="wine-store-details">State:</span>{" "}
-                    {store.state}
-                  </div>
-                  <div>
-                    <span className="wine-store-details">City:</span>{" "}
-                    {store.city}
-                  </div>
+            {/* option 1, still loading from api call */}
+            {wineStoresId !== id ? <Loading></Loading> : <></>}
+
+            {/* option 2, no stores found */}
+            {wineStoresId === id && stores.length === 0 ? (
+              <div>Sorry, no stores found.</div>
+            ) : (
+              <></>
+            )}
+
+            {/* option 3, stores found */}
+            {wineStoresId === id && stores.length > 0 ? (
+              stores.map((store, ind) => {
+                return (
                   <a
-                    className="wine-store-buy"
+                    key={ind}
+                    className="wine-store"
                     href={store.buy_link}
                     target="_blank"
                   >
-                    Buy now
+                    <div>
+                      <span className="wine-store-details">List Item:</span>{" "}
+                      {store.wine_name}
+                    </div>
+                    <div>
+                      <span className="wine-store-details">List Price:</span>{" "}
+                      {store.list_price}
+                    </div>
+                    <div>
+                      <span className="wine-store-details">Store:</span>{" "}
+                      {store.wine_shop}
+                    </div>
+                    <div>
+                      <span className="wine-store-details">State:</span>{" "}
+                      {store.state}
+                    </div>
+                    <div>
+                      <span className="wine-store-details">City:</span>{" "}
+                      {store.city}
+                    </div>
                   </a>
-                </div>
-              );
-            })}
+                );
+              })
+            ) : (
+              <></>
+            )}
           </div>
         </section>
       </div>
